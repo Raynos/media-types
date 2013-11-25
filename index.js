5c0c143d1b2ed409c3c727aacec634b67e9da577
Raynos <raynos2@gmail.com>
2013-11-25 12:37:02 -0800

    set vary header

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Negotiator = require("negotiator")
 
+var varySplitter = / *, */
+
 module.exports = MediaTypes
 
 function MediaTypes(object) {
@@ -16,8 +18,22 @@ function MediaTypes(object) {
         var mediaType = neg.preferredMediaType(types)
 
         var handler = object[mediaType] || $default || defaultHandler
+        vary(req, res, "Accept")
 
         return handler.apply(this, arguments)
+    }
+}
+
+function vary(req, res, header) {
+    var varyHeader = res.getHeader(header)
+    if (varyHeader) {
+        var parts = varyHeader.split(varySplitter)
+        if (parts.indexOf(header) === -1) {
+            parts.push(header)
+        }
+        res.setHeader(header, parts.join(", "))
+    } else {
+        res.setHeader(header, "Accept")
     }
 }
 
